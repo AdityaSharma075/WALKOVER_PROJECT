bfd0f18a0d8996921952487cff696e07b7036107
Aditya Sharma <aditya07543@gmail.com>
2021-10-27 11:56:16 +0530

    heroku ready

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const cookieParser = require('cookie-parser');
 
 const app = express();
 
-const port = 8000;
+const port = process.env.PORT || 8000;
 //encoding the url
 app.use(express.urlencoded());
 app.use(cookieParser());
